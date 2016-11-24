9af18b1dc94852defd210f540e4f46ac8cbc56ff
ZhaoUnruly <android_zzq@163.com>
2016-11-24 15:52:08 +0800

    Hello boys

---
+++ app/src/main/java/com/qiangshijituan/i_love_jianbao/MainActivity.java
@@ -0,0 +1,13 @@
+package com.qiangshijituan.i_love_jianbao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
